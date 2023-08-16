2a7ce21f232ebb8328286d1d07e922bf44b8808c
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-16 15:09:31 +0530

    dependancy changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,30 @@
+import { Fragment, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { LandingLayout } from "@/layout";
+import { LandingNavbar } from "@/containers";
+import { TypingText } from "@/components";
 function Homepage() {
-	return <div> Hello Universa</div>;
+	return (
+		<Fragment>
+			<LandingNavbar></LandingNavbar>
+			<LandingLayout>
+				<div className="homepage">
+					<div className="homepage-container">
+						<h1 className="company-name">Welcome To UNIVERSA</h1>
+						<h2 className="company-slogan">Chase Your Dream</h2>
+						<div className="typing-text-container">
+							<main className="typing-text-main">
+								<TypingText />
+							</main>
+						</div>
+					</div>
+					<div className="homepage-right"></div>
+				</div>
+			</LandingLayout>
+		</Fragment>
+	);
 }
 
 export default Homepage;
