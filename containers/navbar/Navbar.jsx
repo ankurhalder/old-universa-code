c32789f71283006b48fadb6e0c86a71446521597
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-18 18:35:25 +0530

    navbar initial scss added

--- containers/navbar/Navbar.jsx
+++ containers/navbar/Navbar.jsx
@@ -4,16 +4,20 @@ import { useRouter } from "next/router";
 
 function Navbar() {
 	const [showMenu, setShowMenu] = useState(false);
-	const toggleMenu = () => {
-		setShowMenu(!showMenu);
-	};
-
 	const router = useRouter();
 	const [activePage, setActivePage] = useState("/");
 
 	useEffect(() => {
 		setActivePage(router.pathname);
 	}, [router.pathname]);
+
+	const toggleMenu = () => {
+		setShowMenu(!showMenu);
+	};
+
+	const closeMenu = () => {
+		setShowMenu(false);
+	};
 
 	return (
 		<header>
@@ -32,56 +36,72 @@ function Navbar() {
 					<span className="hamburger-icon"></span>
 					<span className="hamburger-icon"></span>
 				</div>
-				<div
-					className={`header-panel ${
-						showMenu ? "active-panel" : "inactive-panel"
-					}`}
-					onClick={toggleMenu}
-				>
-					<div className="hamburger-menu-panel">
-						<ul className="header-panel-sub-menu">
-							<li key="home">
-								<span className={activePage === "/" ? "active" : "notactive"}>
-									<Link href="/">Home</Link>
-								</span>
-							</li>
-							<li key="about">
-								<span
-									className={activePage === "/aboutus" ? "active" : "notactive"}
-								>
-									<Link href="/aboutus">About Us</Link>
-								</span>
-							</li>
-							<li key="services">
-								<span
-									className={
-										activePage === "/services" ? "active" : "notactive"
-									}
-								>
-									<Link href="/services">Services</Link>
-								</span>
-							</li>
-							<li key="contact">
-								<span
-									className={
-										activePage === "/contactus" ? "active" : "notactive"
-									}
-								>
-									<Link href="/contactus">Contact Us</Link>
-								</span>
-							</li>
-							<li key="register">
-								<span
-									className={
-										activePage === "/register" ? "active" : "notactive"
-									}
-								>
-									<Link href="/register">Register</Link>
-								</span>
-							</li>
-						</ul>
+				{showMenu && (
+					<div
+						className={`header-panel ${
+							showMenu ? "active-panel" : "inactive-panel"
+						}`}
+					>
+						<div className="hamburger-menu-panel">
+							<ul className="header-panel-sub-menu">
+								<li key="home">
+									<span
+										className={
+											activePage === "/applicant" ? "active" : "notactive"
+										}
+									>
+										<Link href="/applicant">Home</Link>
+									</span>
+								</li>
+								<li key="about">
+									<span
+										className={
+											activePage === "/applicant/aboutus"
+												? "active"
+												: "notactive"
+										}
+									>
+										<Link href="/applicant/aboutus">About Us</Link>
+									</span>
+								</li>
+								<li key="services">
+									<span
+										className={
+											activePage === "/applicant/services"
+												? "active"
+												: "notactive"
+										}
+									>
+										<Link href="/applicant/services">Services</Link>
+									</span>
+								</li>
+								<li key="contact">
+									<span
+										className={
+											activePage === "/applicant/contactus"
+												? "active"
+												: "notactive"
+										}
+									>
+										<Link href="/applicant/contactus">Contact Us</Link>
+									</span>
+								</li>
+								<li key="register">
+									<span
+										className={
+											activePage === "/applicant/update"
+												? "active"
+												: "notactive"
+										}
+									>
+										<Link href="/applicant/update">Update Profile</Link>
+									</span>
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
+				)}
+				{showMenu && <div className="overlay" onClick={closeMenu}></div>}
 			</div>
 		</header>
 	);
