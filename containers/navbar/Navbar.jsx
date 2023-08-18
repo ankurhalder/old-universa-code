df4c846216a6dd137d3d582c8949e8ccaf864554
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-18 19:12:00 +0530

    new navbar added

--- containers/navbar/Navbar.jsx
+++ containers/navbar/Navbar.jsx
@@ -1,5 +1,33 @@
-function Navbar() {
-	return <div></div>;
-}
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	return (
+		<div className="navbar">
+			<div className="logo">Your Logo</div>
+			<div className="menuButton" onClick={toggleMenu}>
+				{/* Use the FontAwesomeIcon component */}
+				<FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+			</div>
+			{menuOpen && (
+				<div className="menuPanel">
+					<ul className="subMenu">
+						<li>Sub-Item 1</li>
+						<li>Sub-Item 2</li>
+						<li>Sub-Item 3</li>
+						{/* Add more sub-items as needed */}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Navbar;
