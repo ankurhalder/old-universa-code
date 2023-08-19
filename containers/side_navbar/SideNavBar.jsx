7cb67e24b2305ddba7ab13ae021699b4d3a7e6d3
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-19 16:26:14 +0530

    new links added in sidenavbar

--- containers/side_navbar/SideNavBar.jsx
+++ containers/side_navbar/SideNavBar.jsx
@@ -1,44 +1,7 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faBars,
-	faTimes,
-	faQrcode,
-	faLink,
-	faFacebook,
-	faTwitter,
-} from "@fortawesome/free-solid-svg-icons";
-
-const Sidebar = ({ isOpen, toggleSidebar }) => {
-	return (
-		<div className={`side_bar ${isOpen ? "active" : ""}`}>
-			<div className="title">
-				<div className="logo">CodingLab</div>
-				<label className="button cancel" htmlFor="check">
-					{/* <FontAwesomeIcon icon={faTimes} /> */}
-				</label>
-			</div>
-			<ul>
-				<li>
-					<a href="#">
-						{/* <FontAwesomeIcon icon={faQrcode} /> */}
-						Dashboard
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						{/* <FontAwesomeIcon icon={faLink} /> */}
-						Shortcuts
-					</a>
-				</li>
-			</ul>
-			<div className="media_icons">
-				<a href="#">{/* <FontAwesomeIcon icon={faFacebook} /> */}</a>
-				<a href="#">{/* <FontAwesomeIcon icon={faTwitter} /> */}</a>
-			</div>
-		</div>
-	);
-};
+import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const SideNavBar = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -51,10 +14,44 @@ const SideNavBar = () => {
 		<div className="side-navbar">
 			<input type="checkbox" id="check" />
 			<label className="button bars" htmlFor="check" onClick={toggleSidebar}>
-				{/* <FontAwesomeIcon icon={faBars} /> */}
+				<FontAwesomeIcon icon={faBars} />
 			</label>
 
-			<Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+			<div className={`side_bar ${isOpen ? "active" : ""}`}>
+				<div className="title">
+					<div className="logo">UNIVERSA</div>
+					<label className="button cancel" htmlFor="check">
+						<FontAwesomeIcon icon={faXmark} />
+					</label>
+				</div>
+				<ul>
+					<li>
+						<div>
+							<Link href="/applicant">Home</Link>
+						</div>
+					</li>
+					<li>
+						<div>
+							<Link href="/applicant/update">Update Profile</Link>
+						</div>
+					</li>
+					<li>
+						<div>
+							<Link href="/applicant/aboutus">About Us</Link>
+						</div>
+					</li>
+					<li>
+						<div>
+							<Link href="/applicant/contactus">Contact Us</Link>
+						</div>
+					</li>
+					<li>
+						<div>
+							<Link href="/applicant/services">Services</Link>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
