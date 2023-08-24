410e255998a9a02f9061e567ee1e970331922320
Ankur Halder <ankur.halder12345@gmail.com>
2023-08-24 23:34:07 +0530

    password verification added

---
+++ hooks/changePass.jsx
@@ -0,0 +1,50 @@
+async function initiateChangePass() {
+	try {
+		const response = await fetch(
+			"https://universa-api-gateway.onrender.com/services/account/pass/initiate",
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: "email.com",
+				}),
+			}
+		);
+
+		const responseData = await response.json();
+		console.log(responseData);
+	} catch (error) {
+		console.log(error);
+	}
+}
+//  change the old password to new password
+async function updatePass() {
+	try {
+		const response = await fetch(
+			"https://universa-api-gateway.onrender.com/services/account/pass/update",
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: "email.com",
+				}),
+			}
+		);
+
+		const responseData = await response.json();
+		console.log(responseData);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+// todo
+// @{
+//  @   "email": "test1@gmail.com",
+//   @  "password": "NewP@20256",
+//   @  "confirm_password": "NewP@20256"
+//  @ }
